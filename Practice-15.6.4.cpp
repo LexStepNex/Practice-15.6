d4901883335485644280d72fb926dffb5fb3828b
Alexandr <sasha_stepanov94@mail.ru>
2024-06-12 19:29:22 +0300

    Final

--- Practice-15.6.4.cpp
+++ Practice-15.6.4.cpp
@@ -2,36 +2,9 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> inputNumVec(std::vector<int> vec) {
-  int n;
-  do {
-    std::cout << "Input size array: ";
-    std::cin >> n;
-    if (n < 1) {
-      std::cerr << "Array size must be greater than 0\n";
-    }
-  } while (n < 1);
-
-  std::cout << "Input numbers in array: \n";
-
-  for (int i = 0; i < n; i++) {
-    int a;
-    std::cin >> a;
-    vec.push_back(a);
-  }
-
-  return vec;
-}
-
 void printVec(std::vector<int> vec) {
   for (int i = 0; i < vec.size(); i++) {
     std::cout << vec[i] << (i == vec.size() - 1 ? "\n" : " ");
-  }
-}
-
-void printVecReversed(std::vector<int> vec) {
-  for (int i = vec.size() - 1; i >= 0; i--) {
-    std::cout << vec[i] << (i == 0 ? "\n" : " ");
   }
 }
 
